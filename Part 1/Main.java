c3573937c815ed87aaf33bf88d47ce722b1cf5d3
Keyla Morales <115378005+keylamorales@users.noreply.github.com>
2023-03-15 18:57:27 -0500

    Add files via upload

---
+++ Part 1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)  {
+        int result = 0;
+        result = suma(3, 8,9);
+        System.out.println(result);
+    }
+    public static int suma(int a, int b, int c) {
+       return a + b + c;
+    }
+}
